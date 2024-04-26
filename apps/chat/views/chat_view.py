90f8b70ab59eca79b2e37b70b941379583cd7aa1
André Dias <andremssdias@hotmail.com>
2024-04-26 18:46:05 +0200

    feat(chat): Add GET request to chat view

--- apps/chat/views/chat_view.py
+++ apps/chat/views/chat_view.py
@@ -1,9 +1,18 @@
+from django.conf import settings
+
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.views import View
 
 
 class ChatView(View):
+    def get(self, request):
+        username = self.request.COOKIES.get("username")
+        if not username:
+            return redirect("chat:home")
+
+        return render(request, "chat.html", context={"username": username})
+
     def post(self, request):
         username = request.POST.get("username")
         if not username:
@@ -11,5 +20,6 @@ class ChatView(View):
             # Redirect to home page
             return redirect("chat:home")
 
-        return render(request, "chat.html", context={"username": username})
-
+        response = render(request, "chat.html", context={"username": username})
+        response.set_cookie("username", username, httponly=True, secure=settings.COOKIES_SECURE)
+        return response
