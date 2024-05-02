7bed08d1460b16145e02a6ddcb26e7993966d2b2
André Dias <andremssdias@hotmail.com>
2024-05-03 00:31:06 +0200

    feat(chat_connect): Create new ChatSocket JS class

---
+++ chat_connect/static/js/chatSocket.js
@@ -0,0 +1,40 @@
+class ChatSocket extends WebSocket {
+
+    constructor(url, chatView) {
+        super(url);
+        this.chatView = chatView;
+        this.onmessage = (event) => this.handleMessage(event);
+        this.onclose = (event) => this.handleClose(event);
+    }
+
+    handleMessage(event) {
+        const data = JSON.parse(event.data);
+        console.log(`DATA: ${data}`);
+        console.log(data);
+
+        // Display how many users are online
+        if (data.users_online) {
+            console.log(`Users online: ${data.users_online}`);
+            return;
+        }
+
+        // If message was from the same user who sent,
+        // we change the background knowing the user that it was successfully sent
+        if (data.username === username) {
+            this.chatView.updateCurrentUserBackgroundMessage(data.message);
+
+        } else {
+            // Show message from other users
+            this.chatView.displayOtherUserMessage(
+                data.username,
+                data.message
+            );
+        }
+
+    };
+
+    handleClose(e) {
+        console.error("Chat socket closed unexpectedly");
+    };
+
+}
