7e161c04dfc4f70ff3e267a6bfcb48991b74745f
André Dias <andremssdias@hotmail.com>
2024-07-25 18:39:28 +0200

    Fix urls

--- apps/chat/urls.py
+++ apps/chat/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from apps.chat.views.chat_view import ChatView
-from apps.chat.views.close_chat_view import CloseChatView
-from apps.chat.views.home_chat_view import HomeChatView
+from apps.chat.views.chat import ChatView
+from apps.chat.views.close_chat_session import CloseChatSessionView
+from apps.chat.views.home_chat import HomeChatView
 
 app_name = "chat"
 
 urlpatterns = [
     path("", HomeChatView.as_view(template_name="index.html"), name="home"),
     path("live-chat/", ChatView.as_view(), name="live-chat"),
-    path("close-chat/", CloseChatView.as_view(), name="close-chat"),
+    path("close-chat/", CloseChatSessionView.as_view(), name="close-chat"),
 ]
