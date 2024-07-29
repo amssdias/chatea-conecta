2641133156dd2579fd5a5fdfe45ed9ad80295df6
André Dias <andremssdias@hotmail.com>
2024-07-30 00:06:53 +0200

    command(chat): Create command for topics and messages

---
+++ apps/chat/management/commands/create_topics_and_messages.py
@@ -0,0 +1,34 @@
+import json
+
+from django.core.management.base import BaseCommand
+from apps.chat.models import Topic, ConversationFlow
+
+
+class Command(BaseCommand):
+    help = "Creates topics and messages"
+    file_path = "apps/chat/management/data/topic_messages.json"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def handle(self, *args, **options):
+        with open(self.file_path, "r", encoding="utf-8") as file:
+            # Load the JSON data
+            data = json.load(file)
+
+        for topic_name, messages in data.items():
+
+            topic, created = Topic.objects.get_or_create(name=topic_name)
+
+            conversation_topics = []
+            for message in messages:
+                conversation_topics.append(
+                    ConversationFlow(topic=topic, message=message)
+                )
+
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f"Created {len(conversation_topics)} messages of topic -> {topic.name}"
+                )
+            )
+            ConversationFlow.objects.bulk_create(conversation_topics)
