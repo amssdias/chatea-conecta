62aeceb0013dfad495800192f2e923e366f312aa
André Dias <andremssdias@hotmail.com>
2024-08-07 23:59:46 +0200

    config: Update settings variables for tests.

--- chat_connect/settings/settings_tests.py
+++ chat_connect/settings/settings_tests.py
@@ -1,15 +1,19 @@
 from chat_connect.settings.base import *
 
-REDIS_PASSWORD = "redis-test"
-REDIS_PORT = "6380"
-REDIS_HOST = "my-redis-tests"
-REDIS_CHANNEL_LAYER_URL = f"{REDIS_PROTOCOL}://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
 
+# Web Socket - Channels
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [REDIS_CHANNEL_LAYER_URL],  # Reference the test Redis service
-        },
-    },
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
 }
+
+# Use in-memory cache for testing (Django cache)
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+    }
+}
+
+# Use an in-memory broker URL if using Celery
+CELERY_BROKER_URL = "memory://localhost"
