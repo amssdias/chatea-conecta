c0b9c1293a027409295bc3e3606c63cdfce407e5
Dias <andremssdias@hotmail.com>
2024-10-10 23:18:58 +0200

    settings: Update DB to mySQL

--- chat_connect/settings/settings_development.py
+++ chat_connect/settings/settings_development.py
@@ -6,6 +6,17 @@ AUTH_PASSWORD_VALIDATORS = []
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = []
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT", "3306"),
+    }
+}
+
 # Used for auto reload CSS and HTML
 INSTALLED_APPS += ["django_browser_reload"]
 MIDDLEWARE += ["django_browser_reload.middleware.BrowserReloadMiddleware"]
