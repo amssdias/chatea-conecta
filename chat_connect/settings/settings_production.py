dfeaa04f70c6e7bcfd3d8c4ff84d84bb4d1c4e35
Dias <andremssdias@hotmail.com>
2024-10-14 23:01:14 +0200

    settings: Update production settings.

--- chat_connect/settings/settings_production.py
+++ chat_connect/settings/settings_production.py
@@ -1,11 +1,20 @@
 from .base import *
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = []
+
+# Django security settings
 SECURE_SSL_REDIRECT = True
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 300  # 1 year after testing
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = False
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = "DENY"
+SECURE_REFERRER_POLICY = "same-origin"
+
 
 # Channels
 CHANNEL_LAYERS = {
@@ -16,5 +25,9 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+# Time in minutes to expire messages sent
+CLEAR_MESSAGES_EXPIRATION_TIME = 10
+CLEAR_USER_SENT_MESSAGES_TASK_INTERVAL_SCHEDULE_MINUTES = 10
 
 COOKIES_SECURE = True
