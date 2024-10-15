4e3c796652a0d26e5af7afeb11a0235e863f0596
Dias <andremssdias@hotmail.com>
2024-10-15 11:00:13 +0200

    feat: Create task to delete user old messages

--- apps/chat/tasks/__init__.py
+++ apps/chat/tasks/__init__.py
@@ -1,5 +1,7 @@
+from .clear_messages import clear_user_old_messages
 from .send_random_chat_messages import send_random_messages
 
 __all__ = [
     "send_random_messages",
+    "clear_user_old_messages",
 ]
