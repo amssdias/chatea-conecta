4ec71b630da5f8bc42050d1f6c9a9284e44edad1
Dias <andremssdias@hotmail.com>
2024-11-01 18:01:27 +0100

    refact(chat): Remove unused method from cache service

--- apps/chat/services/django_cache_service.py
+++ apps/chat/services/django_cache_service.py
@@ -73,27 +73,6 @@ class DjangoCacheService:
             cache.set(cache_key, username, timeout=settings.CACHE_TIMEOUT_ONE_MONTH)
         return username
 
-    @staticmethod
-    def get_or_create_cache(
-        cache_key, model_class, get_or_create_kwargs, timeout=60 * 60 * 24
-    ):
-        """
-        Retrieves an object from the cache or gets it from the database and sets the cache.
-
-        :param cache_key: Key to identify the object in cache.
-        :param model_class: Django model class to query.
-        :param get_or_create_kwargs: Dict of fields to use with get_or_create.
-        :param timeout: Cache timeout in seconds.
-        :return: Model instance.
-        """
-        obj = cache.get(cache_key)
-
-        if not obj:
-            obj, _ = model_class.objects.get_or_create(**get_or_create_kwargs)
-            cache.set(cache_key, obj, timeout)
-
-        return obj
-
     def has_user_sent_message(
         self, user_id: int, topic_id: int, message_id: int
     ) -> bool:
