d221e165d1996e2a04ea2a7cdfcb275a6f6cdd57
Dias <andremssdias@hotmail.com>
2024-11-03 14:25:54 +0100

    config: Update production settings

--- chat_connect/settings/settings_production.py
+++ chat_connect/settings/settings_production.py
@@ -3,14 +3,29 @@ from .base import *
 DEBUG = False
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 
-# Django security settings
+# ====== Security for HTTPS Enforcement ======
+# - SECURE_SSL_REDIRECT: Redirects all HTTP traffic to HTTPS, ensuring encrypted connections across the site.
+# - SECURE_HSTS_SECONDS: Instructs browsers to remember to only connect to the site over HTTPS for the specified duration (1 year in seconds).
+# - SECURE_HSTS_PRELOAD: Signals the site's readiness for inclusion in browser preload lists, enforcing HTTPS globally without needing an initial visit.
+# - SECURE_HSTS_INCLUDE_SUBDOMAINS: Extends the HTTPS requirement to all subdomains, ensuring they also enforce HTTPS.
 SECURE_SSL_REDIRECT = True
-CSRF_COOKIE_SECURE = True
-CSRF_TRUSTED_ORIGINS = []
-SESSION_COOKIE_SECURE = True
-SECURE_HSTS_SECONDS = 300  # 1 year after testing
+SECURE_HSTS_SECONDS = 31536000  # 1 year
 SECURE_HSTS_PRELOAD = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS = False
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
+# ====== Cookie Security Settings ======
+# - CSRF_COOKIE_SECURE: Ensures the CSRF cookie is only sent over HTTPS, protecting it from exposure over unencrypted connections.
+# - SESSION_COOKIE_SECURE: Marks session cookies as HTTPS-only, preventing them from being sent over unsecured HTTP connections.
+# - CSRF_TRUSTED_ORIGINS: Specifies trusted origins for cross-origin requests with CSRF protection.
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_TRUSTED_ORIGINS = []
+
+# ====== Header Security Settings ======
+# - SECURE_BROWSER_XSS_FILTER: Enables the X-XSS-Protection header in compatible browsers to help prevent cross-site scripting (XSS) attacks.
+# - SECURE_CONTENT_TYPE_NOSNIFF: Adds the X-Content-Type-Options header to prevent browsers from trying to guess the content type, reducing exposure to certain attack vectors.
+# - X_FRAME_OPTIONS: Sets the X-Frame-Options header to "DENY", blocking the site from being embedded in iframes and protecting against clickjacking attacks.
+# - SECURE_REFERRER_POLICY: Limits the referrer information sent in requests to the same origin only, reducing the potential for sensitive information leakage.
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS = "DENY"
@@ -30,7 +45,6 @@ CHANNEL_LAYERS = {
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,  # Ensure other Django loggers remain active
-    
     # Define the format of log messages
     "formatters": {
         "verbose": {
@@ -38,7 +52,6 @@ LOGGING = {
             "style": "{",
         },
     },
-
     # Define where the log messages are sent
     "handlers": {
         "sentry": {
@@ -60,7 +73,6 @@ LOGGING = {
             "formatter": "verbose",
         },
     },
-
     # Define the loggers themselves
     "loggers": {
         "django": {  # Django core logs
