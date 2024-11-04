d807e9d1ffcad0d32f6ba1cacdfcbfc25f40a6e3
Dias <andremssdias@hotmail.com>
2024-11-04 23:53:11 +0100

    fix(users): Inherit correct User manager

--- apps/users/models/managers/user_manager.py
+++ apps/users/models/managers/user_manager.py
@@ -1,10 +1,10 @@
-from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import UserManager
 
 from apps.users.models import Profile
 
 
-class CustomUserManager(BaseUserManager):
+class CustomUserManager(UserManager):
     def create_user_with_profile(self, username, profile_data=None, **extra_fields):
-        user = self.create(username=username, **extra_fields)
+        user = self.create_user(username=username, **extra_fields)
         Profile.objects.create(user=user, **(profile_data or {}))
         return user
