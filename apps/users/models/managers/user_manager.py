f15737019062b5b6f45ad8fe9c15e91f5f3d45ad
Dias <andremssdias@hotmail.com>
2024-11-18 22:57:48 +0100

    feat(chat): Create docstring to explain the creation of a UserManager.

--- apps/users/models/managers/user_manager.py
+++ apps/users/models/managers/user_manager.py
@@ -5,6 +5,7 @@ from apps.users.models import Profile
 
 class CustomUserManager(UserManager):
     def create_user_with_profile(self, username, profile_data=None, **extra_fields):
+        """We create this manager with this method, because with signals we cannot pass arguments in a easy way"""
         user = self.create_user(username=username, **extra_fields)
         Profile.objects.create(user=user, **(profile_data or {}))
         return user
