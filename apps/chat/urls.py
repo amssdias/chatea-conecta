897f9dfc18601465c454344914564f5ceaf9b94b
Dias <andremssdias@hotmail.com>
2024-11-21 14:44:12 +0100

    design: Create privacy policy page

--- apps/chat/urls.py
+++ apps/chat/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from apps.chat.views.chat import ChatView
 from apps.chat.views.close_chat_session import CloseChatSessionView
 from apps.chat.views.home_chat import HomeChatView
 
+
 app_name = "chat"
 
 urlpatterns = [
     path("", HomeChatView.as_view(), name="home"),
+    path("faq/", TemplateView.as_view(template_name="pages/faq.html"), name="faq"),
+    path("privacy-policy/", TemplateView.as_view(template_name="pages/privacy.html"), name="privacy"),
     path("live-chat/", ChatView.as_view(), name="live-chat"),
     path("close-chat/", CloseChatSessionView.as_view(), name="close-chat"),
 ]
