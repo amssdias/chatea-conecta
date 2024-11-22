99531e2febb7649e630203442d1ed4174978171d
Dias <andremssdias@hotmail.com>
2024-11-22 18:11:50 +0100

    feat(chat): Add custom User model to admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 
-from apps.users.models import Profile
+from apps.users.models import Profile, User
 
 
 @admin.register(Profile)
 class TopicAdmin(admin.ModelAdmin):
-    list_display = ("user", "gender")
+    list_display = ("id", "user", "gender")
     search_fields = ("user__username",)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "email")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    list_display = ("id", "username", "email", "first_name", "last_name", "is_staff")
+    search_fields = ("username", "email", "first_name", "last_name")
