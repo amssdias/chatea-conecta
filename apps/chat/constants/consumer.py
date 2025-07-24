e3f1c0814560381351acaa4dee6e911f97659b14
Dias <andremssdias@hotmail.com>
2025-07-25 00:10:00 +0200

    feat(chat): create consumer constants

---
+++ apps/chat/constants/consumer.py
@@ -0,0 +1,8 @@
+USER_GROUP_NOTIFICATION = "user_notification_{}"
+USER_PRIVATE_GROUP = "private_{}_{}"
+
+# WebSocket actions
+REGISTER_GROUP = "register_group"
+SEND_MESSAGE = "send_message"
+PRIVATE_INVITE = "private_invite"
+NOTIFY_USERS_COUNT = "notify_users_count"
