083f4da99c8025aadba09d6697e40f87c39476db
Dias <andremssdias@hotmail.com>
2025-07-25 00:16:46 +0200

    feat(chat): create handler methods for chatSocker controller

--- apps/chat/static/js/chatSocket.js
+++ apps/chat/static/js/chatSocket.js
@@ -6,39 +6,99 @@ class ChatSocket extends WebSocket {
         this.sideMenuView = sideBarView;
         this.onmessage = (event) => this.handleMessage(event);
         this.onclose = (event) => this.handleClose(event);
+
+        this.messageHandlers = {
+            notify_users_count: this.handleNotifyUsersCount.bind(this),
+            send_message: this.handleSendMessage.bind(this),
+            private_invite: this.handleChatInvite.bind(this),
+        };
     }
 
     handleMessage(event) {
         const data = JSON.parse(event.data);
 
-        // Display how many users are online
-        if (data.users_online) {
-            this.chatView.displayNUsersOnline(data.users_online);
-            this.sideMenuView.updateUsersCountOnline(data.users_online);
-            return;
-        }
+        const handler = this.messageHandlers[data.type];
 
-        // If message was from the same user who sent,
-        // we change the background knowing the user that it was successfully sent
-        if (data.username === username) {
-            this.chatView.updateCurrentUserBackgroundMessage(data.message);
-
+        if (handler) {
+            handler(data);
         } else {
-            // Show message from other users
-            this.chatView.displayOtherUserMessage(
-                data.username,
-                data.message,
-                data.groupChatName
-            );
+            console.warn("No handler for message type:", data.type);
         }
 
     };
 
-   handleClose(e) {
-       console.error("Chat socket closed unexpectedly");
-       const chatClosedEl = document.getElementById("chat-closed");
-       chatClosedEl.classList.remove("hide");
-   };
+    handleClose(e) {
+        console.error("Chat socket closed unexpectedly");
+        const chatClosedEl = document.getElementById("chat-closed");
+        chatClosedEl.classList.remove("hide");
+    };
+
+    handleNotifyUsersCount(data) {
+        const onlineUsersCount = data.users_online;
+        this.chatView.displayNUsersOnline(onlineUsersCount);
+        this.sideMenuView.updateUsersCountOnline(onlineUsersCount);
+    }
+
+    handleSendMessage(data) {
+        if (data.username === currentUser) {
+            this.chatView.updateCurrentUserBackgroundMessage(data.message);
+        } else {
+            this.chatView.displayOtherUserMessage(
+                data.username,
+                data.message,
+                data.groupChatName,
+                this.createPrivateChatGroup.bind(this),
+                this.sendMessage.bind(this),
+            );
+        }
+    }
+
+    handleChatInvite(data) {
+        this.chatView.addPrivateChatUser(data);
+    }
+
+    registerGroupUser(groupChatName) {
+        this.send(
+            JSON.stringify({
+                "type": "register_group",
+                "group": groupChatName,
+            })
+        );
+    }
+
+    createMainChat(groupChatName) {
+        this.registerGroupUser(groupChatName);
+
+        // Display chat with event
+        const chat = this.chatView.createChat(
+            groupChatName, 
+            this.sendMessage.bind(this)
+        );
+
+        this.chatView.displayChat(chat);
+    }
+
+    sendMessage(groupName, message) {
+        this.send(
+            JSON.stringify({
+                "type": "send_message",
+                "group": groupName,
+                "message": message
+            })
+        );
+    }
+
+    createPrivateChatGroup(username) {
+        this.chatView.openPrivateChatModal(
+            username,
+            this.sendMessage.bind(this),
+        );
+
+        this.send(JSON.stringify({
+            "type": "private_invite",
+            "target_user_id": username
+        }));
+    }
 
 }
 
