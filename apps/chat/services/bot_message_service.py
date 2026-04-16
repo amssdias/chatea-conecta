787bb109498e98eb1a9dc31c4517c1507cd86dfa
Dias <andremssdias@hotmail.com>
2026-04-17 00:21:19 +0200

    fix(chat): change sync to async to regular flow

--- apps/chat/services/bot_message_service.py
+++ apps/chat/services/bot_message_service.py
@@ -1,5 +1,3 @@
-from asgiref.sync import sync_to_async
-
 from apps.chat.constants.redis_keys import TASK_LOCK_KEY
 from apps.chat.infrastructure.redis.async_redis_service import AsyncRedisService
 from apps.chat.tasks.send_random_chat_messages import send_random_messages
@@ -14,4 +12,4 @@ async def start_bot_messages_task(group: str):
     """
     task_lock = await AsyncRedisService.set_if_not_exists(TASK_LOCK_KEY, "locked")
     if task_lock:
-        await sync_to_async(send_random_messages.delay)(group)
+        send_random_messages.delay(group)
