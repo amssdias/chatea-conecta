a9a5cf35768a099a52d7d275555964afc1f55948
Dias <andremssdias@hotmail.com>
2026-05-24 20:26:48 +0200

    fix(users): update redis on login and register

--- apps/users/views/login.py
+++ apps/users/views/login.py
@@ -2,8 +2,7 @@ from django.conf import settings
 from django.contrib.auth.views import LoginView
 from django.urls import reverse
 
-from apps.chat.constants.redis_keys import REDIS_ALL_USERNAMES_KEY, ID_TO_USERNAME_KEY, USERNAME_TO_UUID_KEY
-from apps.chat.infrastructure.redis.sync_redis_service import RedisService
+from apps.chat.services.register_user import register_user_on_redis
 
 
 class CustomLoginView(LoginView):
@@ -17,7 +16,7 @@ class CustomLoginView(LoginView):
         username = user.username
         user_id = str(user.pk)
 
-        self._prepare_chat_user(username=username, user_id=user_id)
+        register_user_on_redis(username=username, user_id=user_id)
 
         response.set_cookie(
             "username",
@@ -36,8 +35,3 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse("chat:live-chat")
-
-    def _prepare_chat_user(self, username: str, user_id: str):
-        RedisService.add_to_set(REDIS_ALL_USERNAMES_KEY, username.lower())
-        RedisService.set_unique(ID_TO_USERNAME_KEY.format(user_id=user_id), username)
-        RedisService.set_unique(USERNAME_TO_UUID_KEY.format(username=username), user_id)
