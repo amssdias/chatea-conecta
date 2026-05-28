f06565a96a68987c50a3049bc357316d21e1665d
Dias <andremssdias@hotmail.com>
2026-05-28 23:30:41 +0200

    feat(subscriptions): update nav bar based on user subscription

--- apps/users/models/user.py
+++ apps/users/models/user.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 
+from apps.subscriptions.models.choices import UserSubscriptionStatus
 from apps.users.models.managers.user_manager import CustomUserManager
 
 
@@ -8,3 +9,11 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+    @property
+    def is_pro(self):
+        return self.subscription.pro
+
+    @property
+    def needs_payment_configuration(self):
+        return self.subscription.status == UserSubscriptionStatus.PAST_DUE
