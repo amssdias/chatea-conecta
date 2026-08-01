e5bd9b3cb59b3942cefd39fc56da033ec95d0787
Dias <andremssdias@hotmail.com>
2026-08-01 15:01:54 +0200

    feat(checkout): integrate subscription retrieval and synchronization from Stripe checkout session

--- apps/subscriptions/webhook_handlers/checkout.py
+++ apps/subscriptions/webhook_handlers/checkout.py
@@ -1,10 +1,16 @@
 from stripe.checkout import Session
 
-from apps.subscriptions.services.user_subscription import save_stripe_subscription_id
+from apps.integrations.stripe.subscriptions import retrieve_subscription
+from apps.subscriptions.services.user_subscription import (
+    sync_user_subscription_from_checkout,
+)
+from apps.subscriptions.webhook_handlers.mappers import (
+    build_subscription_sync_dto,
+)
 
 
 def handle_checkout_session_completed(session: Session):
-    save_stripe_subscription_id(
-        stripe_customer_id=session.customer,
-        stripe_subscription_id=session.subscription
-    )
+    subscription = retrieve_subscription(session.subscription)
+    subscription_sync = build_subscription_sync_dto(subscription)
+
+    sync_user_subscription_from_checkout(subscription_sync)
